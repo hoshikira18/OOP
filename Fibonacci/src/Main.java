6a317d2ab892b6e6ec07ed41ddb34a2bd7da8765
Tran Van Khuyen <khuyentvhe187268@fpt.edu.vn>
2024-05-15 18:25:56 +0700

    add: fibonacci project

---
+++ Fibonacci/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Fibonacci fib = new Fibonacci();
+        System.out.println(fib.fibonacci(45));
+        fib.printFibonacci(45);
+    }
+}
